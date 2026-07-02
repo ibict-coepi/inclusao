14d03b544e2f659ba1f1dc491f0b18eb58b2119d
ibict-coepi <coepi@ibict.br>
2026-07-02 11:26:10 -0300

    Set base path to '/inclusao/' in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import {defineConfig} from 'vite';
 const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
 
 export default defineConfig({
-  base: repositoryName ? `/${repositoryName}/` : '/',
+  base: '/inclusao/',
   plugins: [react(), tailwindcss()],
 });
